8a75f9668f10452a876fdbaa57e86de46bace0f7
gavinmcfall <gavin@nerdz.co.nz>
2023-12-16 21:52:12 +1300

    more tweaks

--- terraform/scripts/9001_k3s_control_apply.sh
+++ terraform/scripts/9001_k3s_control_apply.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd "$(dirname "$0")/../9001_k3s_cluster/control"
-terraform apply -auto-approve
+terraform apply -auto-approve "plan.out"
--- terraform/scripts/9001_k3s_control_destroy.sh
+++ terraform/scripts/9001_k3s_control_destroy.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-eval $(op signin)
 cd "$(dirname "$0")/../9001_k3s_cluster/control"
 terraform destroy -auto-approve
